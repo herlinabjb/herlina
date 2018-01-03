85c8fd0b4d42c0e25a884dc116cc5851ac2cf244
hadliani007 <hadliani007@gmail.com>
2018-01-03 21:29:06 +0800

    double

--- saw.cpp
+++ saw.cpp
@@ -3,3 +3,9 @@
 using namespace std;
 
 int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	//x
+	double a1c[5]={4,4,5,3,3}, a2c[5]={3,3,4,2,3}, a3c[5]={5,4,2,2,2};
+	//x
+	cout << "MATRIX X :"<<endl;
+	int kuda1=0;
